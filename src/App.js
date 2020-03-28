66c9938b2d1bb783cf61d73ee4186a1312a59655
Alessio <alessioferriweb@gmail.com>
2020-03-28 18:56:14 +0000

    Adding Routes in the home App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import Rules from "./Rules";
+import Game from "./Game";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <header className="App-header">
+                    <Switch>
+                        <Route path="/game">
+                            <Game />
+                        </Route>
+                        <Route path="/rules">
+                            <Rules />
+                        </Route>
+                        <Route path="/">
+                            <img src={logo} className="App-logo" alt="logo"/>
+                            <p>
+                                Click <Link className="App-link" to="/game">here</Link> to play.
+                                <br/>
+                                Click <Link className="App-link" to="/rules">here</Link> for the rules.
+                            </p>
+                        </Route>
+                    </Switch>
+                </header>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
